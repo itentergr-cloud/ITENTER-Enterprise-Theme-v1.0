07a28eca2ccc144aab499423857f5d8cb8e08ecd
itentergr-cloud <itentergr@gmail.com>
2026-07-23 20:16:37 +0300

    Update front-page.php

--- itenter-enterprise/front-page.php
+++ itenter-enterprise/front-page.php
@@ -1,1 +1,27 @@
+php
+<?php
 
+get_header();
+
+
+get_template_part(
+    'template-parts/hero'
+);
+
+
+get_template_part(
+    'template-parts/services'
+);
+
+
+get_template_part(
+    'template-parts/contact'
+);
+
+
+get_footer();
+
+?>
+
+
+---
