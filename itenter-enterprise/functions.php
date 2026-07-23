81202685ee8949bfd89f5537a1263eb1b07945d2
itentergr-cloud <itentergr@gmail.com>
2026-07-23 18:47:04 +0300

    Update functions.php

--- itenter-enterprise/functions.php
+++ itenter-enterprise/functions.php
@@ -1,1 +1,117 @@
+php
+<?php
 
+/**
+ * ITENTER Enterprise Theme
+ */
+
+
+if(!defined('ABSPATH')){
+
+    exit;
+
+}
+
+
+/**
+ * Theme Setup
+ */
+
+function itenter_theme_setup(){
+
+
+    add_theme_support(
+        'title-tag'
+    );
+
+
+    add_theme_support(
+        'post-thumbnails'
+    );
+
+
+    add_theme_support(
+        'custom-logo',
+        [
+            'height'=>80,
+            'width'=>250,
+            'flex-height'=>true,
+            'flex-width'=>true
+        ]
+    );
+add_theme_support(
+        'html5',
+        [
+            'search-form',
+            'gallery',
+            'caption',
+            'style',
+            'script'
+        ]
+    );
+
+
+    register_nav_menus([
+
+        'primary_menu'=>
+        __('Primary Menu','itenter')
+
+    ]);
+
+}
+
+
+add_action(
+    'after_setup_theme',
+    'itenter_theme_setup'
+);
+
+
+
+/**
+ * Load Assets
+ */
+
+function itenter_assets(){
+
+
+    wp_enqueue_style(
+
+        'itenter-main',
+
+        get_template_directory_uri()
+        .'/assets/css/main.css',
+
+        [],
+
+        '1.0.0'
+      );
+
+
+
+    wp_enqueue_script(
+
+        'itenter-main-js',
+
+        get_template_directory_uri()
+        .'/assets/js/main.js',
+
+        [],
+
+        '1.0.0',
+
+        true
+
+    );
+
+
+}
+
+
+add_action(
+    'wp_enqueue_scripts',
+    'itenter_assets'
+);
+
+
+---
